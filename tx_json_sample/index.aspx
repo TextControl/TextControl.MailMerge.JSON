1c1354181e5919cbe0e1e2711d8547c192b82088
bjoerntx <bjoern@textcontrol.com>
2015-08-04 14:23:15 +0200

    Uploaded sample

---
+++ tx_json_sample/index.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="tx_json_sample.index" %>
+
+<%@ Register assembly="TXDocumentServer, Version=22.0.800.500, Culture=neutral, PublicKeyToken=6b83fe9a75cfb638" namespace="TXTextControl.DocumentServer.Web" tagprefix="cc1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>TX Text Control: Merge from JSON data</title>
+    <script src="Scripts/jquery-2.1.4.js"></script>
+    <link href="DocumentViewer.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="TextBox1" runat="server" Height="332px" TextMode="MultiLine" Width="619px"></asp:TextBox><br />
+
+        <asp:Button ID="Button1" runat="server" Text="Merge" OnClick="Button1_Click" /><br />
+        <asp:Label ID="lblError" runat="server" Text="Label" Visible="False" Font-Names="Arial" ForeColor="Red" /><br />
+        <cc1:DocumentViewer runat="server" ID="DocumentViewer1" Height="332px" Width="624px" />
+
+        <script>
+
+            // display text JSON data in TextBox on every page load
+            var sJsonData = { 'orders':{ 'order':[ { 'Id':'10', 'address':{ 'street':'9774 Kings Drive', 'city':'Charlotte' }, 'phone':'123 898 2298', 'date':'05/12/2015', 'total':'1526.88', 'item':[ { 'name':'Thin-Jam Hex Nut 4', 'price':'337.22', 'qty':'1', 'itemtotal':'337.22' }, { 'name':'ML Road Frame - Red, 58', 'price':'594.83', 'qty':'2', 'itemtotal':'1189.66' } ] } ] } };
+            $("#TextBox1").text(JSON.stringify(sJsonData, null, 4));
+
+        </script>
+    </div>
+        
+    </form>
+</body>
+</html>
---
+++ tx_json_sample/index.aspx.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using TXTextControl;
+using TXTextControl.DocumentServer;
+
+namespace tx_json_sample
+{
+    public partial class index : System.Web.UI.Page
+    {
+        private DocumentController documentController1;
+        private ServerTextControl serverTextControl1;
+        private System.ComponentModel.IContainer components;
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            MergeTemplate("template.tx", TextBox1.Text);
+        }
+
+        private void MergeTemplate(string templateName, string JsonData)
+        {
+            // creating a new ServerTextControl and MailMerge class
+            using (ServerTextControl tx = new ServerTextControl())
+            {
+                tx.Create();
+                MailMerge mailMerge = new MailMerge();
+                mailMerge.TextComponent = tx;
+
+                // load the template
+                mailMerge.LoadTemplate(Server.MapPath("templates/" + templateName),
+                    FileFormat.InternalUnicodeFormat);
+
+                // create a new DataSet object
+                DataSet ds = new DataSet();
+                XmlDocument doc;
+
+                // convert the JSON string to an XML document
+                // object using Newtonsoft.Json
+                try
+                {
+                    doc = JsonConvert.DeserializeXmlNode(JsonData);
+                    lblError.Visible = false;
+                }
+                catch (Exception exc)
+                {
+                    lblError.Visible = true;
+                    lblError.Text = exc.Message;
+                    DocumentViewer1.Visible = false;
+                    return;
+                }
+
+                // convert the XML to a DataSet
+                XmlNodeReader reader = new XmlNodeReader(doc);
+                ds.ReadXml(reader, XmlReadMode.Auto);
+
+                // merge the template with the DataSet
+                mailMerge.Merge(ds.Tables[0]);
+
+                // load the resulting document into the DocumentViewer
+                byte[] data;
+                mailMerge.SaveDocumentToMemory(out data, BinaryStreamType.InternalUnicodeFormat, null);
+                DocumentViewer1.LoadDocumentFromMemory(data, FileFormat.InternalUnicodeFormat);
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.documentController1 = new TXTextControl.DocumentServer.DocumentController(this.components);
+            this.serverTextControl1 = new TXTextControl.ServerTextControl();
+            // 
+            // documentController1
+            // 
+            this.documentController1.TextComponent = this.serverTextControl1;
+            // 
+            // serverTextControl1
+            // 
+            this.serverTextControl1.SpellChecker = null;
+
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            InitializeComponent();
+            DocumentViewer1.DocumentController = documentController1;
+            DocumentViewer1.Visible = true;
+        }
+    }
+}
